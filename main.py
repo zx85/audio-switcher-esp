369b25fa11113197133f0ddfeda90533acadf9ba
james <git@mus-ic.co.uk>
2023-09-01 23:54:35 +0100

    carnage with aasyncio stuff

--- main.py
+++ main.py
@@ -17,8 +17,10 @@
 # Phys Pin In/Out Function
 # D1   5   OUT    SCL - TFT Screen
 # D2   4   OUT    SDA - TFT Screen
-# D3   0   OUT    Motor +
-# D4   2   OUT    Motor -
+##################################
+# NOT D3   0   OUT    Motor + - this enables flash writing
+# D4   2   OUT    Motor -     - so going to have to rethink this
+##################################
 # D5   14  IN     Button (PullUp)
 # D6   12  OUT    Input Selector A
 # D7   13  OUT    Input Selector B
@@ -26,7 +28,9 @@
 
 # References
 # https://www.upesy.com/blogs/tutorials/hardware-interrupts-rpi-pico-on-micropython#
-
+#
+# Webserver - https://github.com/miguelgrinberg/microdot - TODO
+# Async =  https://www.digikey.co.uk/en/maker/projects/getting-started-with-asyncio-in-micropython-raspberry-pi-pico/110b4243a2f544b6af60411a85f0437c
 # Important imports
 import time
 from machine import Pin, I2C
@@ -34,18 +38,24 @@ import ssd1306
 import network
 import sys  
 import os
+import uasyncio
+import usocket
+import utime
+from uselect import select
 
 # Initialise the screen using default address 0x3C
 # i2c = I2C(sda=Pin(4), scl=Pin(5 ))
 # display = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 # Easy To Remember variables
+# Going to pull up so it triggers on LOW
 Btn=Pin(14, Pin.IN, Pin.PULL_UP)
-MotUp=Pin(0, Pin.OUT)
-MotDn=Pin(2, Pin.OUT)
-OutA=Pin(12, Pin.OUT)
-OutB=Pin(13, Pin.OUT)
-OutC=Pin(15, Pin.OUT)
+# Going to have to rethink the motors because D3 can't be used
+# MotUp=Pin(0, Pin.OUT)
+MotDn=Pin(2, Pin.OUT) # This is actually the internal LED. Handy
+OutA=Pin(12, Pin.OUT, Pin.PULL_UP)
+OutB=Pin(13, Pin.OUT, Pin.PULL_UP)
+OutC=Pin(15, Pin.OUT, Pin.PULL_UP)
 
 def file_exists(filename):
     try:
@@ -53,12 +63,6 @@ def file_exists(filename):
         return True
     except OSError:
         return False
-
-def btn_on(pin):
-    print("Button pressed")
-
-def btn_off(pin):
-    print("Button released")
 
 def content_type(uri):
     this_type='text/html'
@@ -82,34 +86,53 @@ def web_page(filename):
         response="404 not found"
     return content,response
 
-def main():
-    # configure an irq callback
-    Btn.irq(trigger=Pin.IRQ_FALLING, handler=btn_off)
-    Btn.irq(trigger=Pin.IRQ_RISING, handler=btn_on)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', 80))
-    s.listen(5)
-    print("Listening on port 80 then")
+# Coroutine: blink on a timer
+async def blink():
+    delay_ms = 500
     while True:
-        conn, addr = s.accept()
-        print('Got a connection from %s' % str(addr))
-        request = conn.recv(1024)
-        request = str(request)
-        print('Content = %s' % request)
-        uri=request.split(" ")[1][1:]
-        print("Bit I want is: "+uri)
-        if uri=="":
-            uri="index.html"
-        content,responsecode = web_page(uri)
-        conn.send('HTTP/1.1 '+responsecode+'\n')
-        conn.send('Content-Type: '+content_type(uri)+'\n')
-        conn.send('Connection: close\n\n')
-        conn.sendall(content)
-        conn.close()
+        MotDn(not MotDn())
+        await uasyncio.sleep_ms(delay_ms)
+
+# Coroutine: only return on button press
+async def wait_button():
+    LONG_THRESHOLD=500
+    btn_press=False
+    long_press=False
+    btn_prev = Btn.value()    
+    timestamp=utime.ticks_ms()        
+    while Btn.value() == 0:
+        btn_press=True
+        if btn_prev==1:
+            timestamp=utime.ticks_ms()
+        if utime.ticks_ms()-timestamp>LONG_THRESHOLD:
+            long_press=True
+        btn_prev = Btn.value()
+    await uasyncio.sleep_ms(40)
+    return btn_press,long_press
+
+async def main():
+
+    addr = usocket.getaddrinfo('0.0.0.0', 80)[0][-1]
+    s = usocket.socket()
+    s.bind(addr)
+    s.listen(1)
+
+    # Start coroutine as a task and immediately return
+    uasyncio.create_task(blink())
+#    uasyncio.create_task(webserver(s))
+    while True:
+#        print("waiting for the button then")
+        btn_press,long_press=await wait_button()
+        if btn_press:
+            if long_press:
+                print("LONG")
+            else:
+                print("SHORT")
 
 if __name__ == "__main__":
 #    try:
-        main()
+# Start event loop and run entry point coroutine
+    uasyncio.run(main())
 #    except KeyboardInterrupt:
 #        pass
